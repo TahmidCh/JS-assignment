f1a228da4f5902e5d1ee95de5ea2c8e095027539
Tahmid Chowdhury <mmhtc1698@gmail.com>
2021-01-22 16:51:20 +0600

    done point four

--- assignment.js
+++ assignment.js
@@ -21,13 +21,13 @@ function budgetCalculator(quantityOfWatch, quantityOfPhone, quantityOfLaptop) {
     var watchPrice = 50;
     var phonePrice = 100;
     var laptopPrice = 500;
-    if (quantityOfWatch < 0 || quantityOfPhone < 0 || quantityOfLaptop < 0 || typeof quantityOfWatch != "number" || typeof quantityOfPhone != "number" || typeof quantityOfLaptop != "number") {
+    if (quantityOfWatch < 0 || quantityOfPhone < 0 || quantityOfLaptop < 0 || typeof quantityOfWatch != "number" || typeof quantityOfPhone != "number" || typeof quantityOfLaptop != "number" || Number.isInteger(quantityOfWatch) == false || Number.isInteger(quantityOfPhone) == false || Number.isInteger(quantityOfLaptop) == false) {
         return "Your input is invalid"
     }
     else {
-        var totalWatchPrice = Math.round(quantityOfWatch) * watchPrice;
-        var totalPhonePrice = Math.round(quantityOfPhone) * phonePrice;
-        var totalLaptopPrice = Math.round(quantityOfLaptop) * laptopPrice;
+        var totalWatchPrice = quantityOfWatch * watchPrice;
+        var totalPhonePrice = quantityOfPhone * phonePrice;
+        var totalLaptopPrice = quantityOfLaptop * laptopPrice;
         var total = totalWatchPrice + totalPhonePrice + totalLaptopPrice;
         return total;
     }
@@ -40,22 +40,22 @@ function budgetCalculator(quantityOfWatch, quantityOfPhone, quantityOfLaptop) {
 
 function hotelCost(numberOfDays) {
     var totalCost = 0;
-    if (numberOfDays < 0 || typeof numberOfDays != "number") {
+    if (numberOfDays < 0 || typeof numberOfDays != "number" || Number.isInteger(numberOfDays)== false) {
         totalCost = "Your input is invalid";
     }
     else if (numberOfDays <= 10) {
-        numberOfDays = Math.round(numberOfDays);
+        numberOfDays = numberOfDays;
         totalCost = numberOfDays * 100;
     }
     else if (numberOfDays <= 20) {
-        numberOfDays = Math.round(numberOfDays);
+        numberOfDays = numberOfDays;
         var firstTenDays = 10 * 100;
         var remaining = numberOfDays - 10;
         var tenToTwentyDays = remaining * 80
         totalCost = firstTenDays + tenToTwentyDays;
     }
     else {
-        numberOfDays = Math.round(numberOfDays);
+        numberOfDays = numberOfDays;
         var firstTenDays = 10 * 100;
         var tenToTwentyDays = 10 * 80;
         var remaining = numberOfDays - 20;
